d8c0a73f28bbf4581ca03a8a66a02bb607902b36
Shreyash Rai <raishreyash1519@gmail.com>
2026-09-24 17:49:33 +0530

    Add authentication middleware

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user.models')
+
+async function userAuth (req, res, next){
+    try{
+            const token  = req.cookies;
+    if(!token){
+        return res.status(400).json({
+            message: "Unautharized access"
+        })
+    }
+
+    const tokenVerify = jwt.verify(token, process.env.JWT_TOKEN)
+    const user = await userModel.findById(tokenVerify.id)
+    if(!user){
+        return res.status(400).json({
+            message : "Unautharized access"
+        })
+    }
+
+    req.user = user
+    next()
+
+    } catch(err){
+        return res.status(401).json({message: "Unauthorized access"})
+    }
+
+}
+
+module.exports = userAuth
