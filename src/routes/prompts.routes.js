c1520090d3994e55fed2ac5a3f9ab43bf0f09c3d
Shreyash Rai <raishreyash1519@gmail.com>
2026-09-25 23:33:11 +0530

    Added the download/:id route

--- src/routes/prompts.routes.js
+++ src/routes/prompts.routes.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const promptController = require('../controllers/prompts.controllers')
 const authMiddleware = require('../middlewares/auth.middlewares')
+const jsonD = require('../controllers/json.controllers')
+
 const router = express.Router()
+
+router.get('/download/:id', authMiddleware.userAuth, jsonD.jsonDownload)
 
 router.post('/create', authMiddleware.userAuth, promptController.createPrompt)
 router.get('/getAll', authMiddleware.userAuth, promptController.getPrompts)
-router.get('/getById', authMiddleware.userAuth, promptController.getPromptById)
-router.patch('/update', authMiddleware.userAuth, promptController.updatePrompt)
-router.delete('/delete', authMiddleware.userAuth, promptController.deletePrompt)
+router.get('/getById/:id', authMiddleware.userAuth, promptController.getPromptById)
+router.patch('/update/:id', authMiddleware.userAuth, promptController.updatePrompt)
+router.delete('/delete/:id', authMiddleware.userAuth, promptController.deletePrompt)
 
 module.exports = router
